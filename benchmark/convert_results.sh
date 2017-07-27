a5147285daaa8820b13f3324b44478c4ca64d4be
Abel Hegedus <abel.hegedus@incquerylabs.com>
2017-07-27 14:30:34 +0200

    fix convert_results script

--- benchmark/convert_results.sh
+++ benchmark/convert_results.sh
@@ -5,7 +5,7 @@ cd "$( cd "$( dirname "$0" )" && pwd )"
 rm -rf ${WORKSPACE}/benchmark/results
 mkdir ${WORKSPACE}/benchmark/results
 
-cd ${WORKSPACE}/com.incquerylabs.instaschema.test/results
+cd ${WORKSPACE}/com.incquerylabs.benchmark.test/results
 
 for i in $(ls -d */); do
   echo ${i};
@@ -15,7 +15,7 @@ for i in $(ls -d */); do
   else
     echo "PROCESS"
 	mkdir ${WORKSPACE}/benchmark/results/${i}
-	python3 ${WORKSPACE}/mondo-sam/reporting/convert_results.py --source ${WORKSPACE}/com.incquerylabs.instaschema.test/results/${i} \
+	python3 ${WORKSPACE}/mondo-sam/reporting/convert_results.py --source ${WORKSPACE}/com.incquerylabs.benchmark.test/results/${i} \
 --jsonfile ${WORKSPACE}/benchmark/results/${i}results.json \
 --csvfile ${WORKSPACE}/benchmark/results/${i}results.csv
   fi
